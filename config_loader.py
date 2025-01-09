10f328445aad9bcf930fb6da03b8c0d9ee7cd247
Nathan Fenwick <nathanfenwick@rocketmail.com>
2025-01-09 11:41:06 +0000

    Added the new PostGreSQL database to settings.py. Added config_loader.py to allow for the load of secret keys from .env. Updated settings.py to the new config_loader.py. Migrated to models over to the database.

---
+++ config_loader.py
@@ -0,0 +1,51 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Config, cls).__new__(cls)
+            cls._instance.load_config()
+        return cls._instance
+
+    def load_config(self):
+        self.__test_flag = os.getenv("GLOBAL_TEST_FLAG", "false")
+        self.__django_secret = os.getenv("DJANGO_SECRET")
+        self.__db_host = os.getenv("DB_HOST")
+        self.__db_port = os.getenv("DB_PORT")
+        self.__db_name = os.getenv("DB_NAME")
+        self.__db_user = os.getenv("DB_USER")
+        self.__db_password = os.getenv("DB_PASSWORD")
+
+    @property
+    def test_flag(self):
+        return self.__test_flag.lower() in ("true", "1", "yes")
+
+    @property
+    def django_secret(self):
+        return self.__django_secret
+
+    @property
+    def db_host(self):
+        return self.__db_host
+
+    @property
+    def db_port(self):
+        return self.__db_port
+
+    @property
+    def db_name(self):
+        return self.__db_name
+
+    @property
+    def db_user(self):
+        return self.__db_user
+
+    @property
+    def db_password(self):
+        return self.__db_password
